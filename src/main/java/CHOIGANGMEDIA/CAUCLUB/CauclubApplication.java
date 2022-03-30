db8afe99c0ec3e18770c4869bf5b24140f5886bc
kitaee <dlrlxo999@naver.com>
2022-03-31 03:52:42 +0900

    🔥First Test Commit

---
+++ src/main/java/CHOIGANGMEDIA/CAUCLUB/CauclubApplication.java
@@ -0,0 +1,14 @@
+package CHOIGANGMEDIA.CAUCLUB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CauclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CauclubApplication.class, args);
+		System.out.println("Hello Spring!");
+	}
+
+}
---
+++ src/test/java/CHOIGANGMEDIA/CAUCLUB/CauclubApplicationTests.java
@@ -0,0 +1,13 @@
+package CHOIGANGMEDIA.CAUCLUB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CauclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
