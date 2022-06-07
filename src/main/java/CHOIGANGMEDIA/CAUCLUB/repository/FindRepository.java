3c042a3dadf1dedc16fd076b8036eabc03676164
kitaee <dlrlxo999@naver.com>
2022-06-07 09:35:33 +0900

    modify ResetPassword API

--- src/main/java/CHOIGANGMEDIA/CAUCLUB/repository/FindRepository.java
+++ src/main/java/CHOIGANGMEDIA/CAUCLUB/repository/FindRepository.java
@@ -2,7 +2,7 @@ package CHOIGANGMEDIA.CAUCLUB.repository;
 
 public interface FindRepository {
     Boolean emailCheck(String email) throws Exception;
-    Boolean resetPassword(String id, String password) throws Exception;
+    Boolean resetPassword(String id, String password, String salt) throws Exception;
     String getIdByEmail(String email) throws Exception;
     Boolean resetPasswordByEmail(String email, String password) throws Exception;
 }
